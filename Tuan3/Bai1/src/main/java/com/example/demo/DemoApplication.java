d2b99fd5cb0f0965e4e23a157a733de0b382ef46
An NK <nka151203@gmail.com>
2025-02-18 19:28:58 +0700

    Bai 1 Done

--- Tuan3/Bai1/src/main/java/com/example/demo/DemoApplication.java
+++ Tuan3/Bai1/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,12 @@
-package com.Excerices.Bai2;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Bai2Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Bai2Application.class, args);
-	}
+public class DemoApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 }
