993c673852e7ae3e84e6b7dcd78429f82a47b8f5
An NK <nka151203@gmail.com>
2025-03-11 17:37:56 +0700

    Add Demo

--- Tuan6/demo (1)/demo/src/main/java/com/example/wso2springdemo/Wso2SpringDemoApplication.java
+++ Tuan6/demo (1)/demo/src/main/java/com/example/wso2springdemo/Wso2SpringDemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Wso2SpringDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Wso2SpringDemoApplication.class, args);
 	}
